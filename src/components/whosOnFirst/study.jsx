c68a142df8d61bfe0e1d470bef363a6b2795f66b
Robert Pooley <robertcpooley@gmail.com>
2020-03-07 20:27:06 -0500

    whos on first part of step 2

--- src/components/whosOnFirst/study.jsx
+++ src/components/whosOnFirst/study.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import P from '../paragraph';
 import H from '../heading';
 import FlexGroup from '../flexGroup';
+import Util from '../../util';
 
 class Study extends React.Component {
     render() {
@@ -31,8 +32,47 @@ class Study extends React.Component {
                 'You are'
             ]
         };
+
+        const group1 = [
+            'Ready',
+            'First',
+            'No',
+            'Blank',
+            'Nothing',
+            'Yes',
+            'What',
+            'Uhhh',
+            'Left',
+            'Right',
+            'Middle',
+            'Okay',
+            'Wait',
+            'Press'
+        ].sort();
+        const group2 = [
+            'You',
+            'You are',
+            'Your',
+            "You're",
+            'UR',
+            'U',
+            'Uh huh',
+            'Uh uh',
+            'What?',
+            'Done',
+            'Next',
+            'Hold',
+            'Sure',
+            'Like'
+        ].sort();
+
         return (
             <div>
+                <P className="text-center">
+                    <a href={Util.manual(9)} target="_blank">
+                        Manual
+                    </a>
+                </P>
                 <P>
                     <H n={1}>Step 1</H>
                     Here are the lists of words sorted by position:
@@ -51,6 +91,31 @@ class Study extends React.Component {
                     I recommend memorizing the shorter ones first since once you
                     memorize everything except for Bottom Right, you know the
                     words you don't recognize are Bottom Right.
+                </P>
+                <P className="list-2c">
+                    <H n={1}>Step 2</H>
+                    There are 28 words that can be split in half into two
+                    groups. The word lists for each group of 14 only pull from
+                    those 14 words.
+                    <P>
+                        <H>Group 1</H>
+                        <div className="d-flex justify-content-center">
+                            <ol>
+                                {group1.map(w => (
+                                    <li key={w}>{w}</li>
+                                ))}
+                            </ol>
+                        </div>
+                        For each word list, I made a sentence where the first
+                        letter of each word corresponds to the first letter of
+                        the word to press. The first word of the sentence
+                        corresponds to the label from step 1. For no/nothing,
+                        ready/right, and wait/what, I tried to use the actual
+                        word in the sentence since it could be ambiguous for
+                        which one to press. I recommend coming up with sentences
+                        that are the easiest for you to memorize, here are the
+                        ones I use:
+                    </P>
                 </P>
             </div>
         );
